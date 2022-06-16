2266b7a02de4115063b59fc920d08f7ff7c883ba
miirage <mika.baeza@laposte.net>
2022-06-16 14:28:48 +0200

    created app

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require('express')
+const http = require('http')
+const port = process.env.PORT
+const { randomUUID } = require('crypto');
+
+const app = express()
+const server = http.createServer(app)
+
+// Préréglages de l'application
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+// Démarre le serveur
+server.listen(port, () => {
+    console.log('App listening on port ' + port)
+})
