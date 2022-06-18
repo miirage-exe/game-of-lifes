b40d635b0f93d5ea0b7c4b96aebf25a122d4cd8a
miirage <mika.baeza@laposte.net>
2022-06-18 21:43:19 +0200

    sound effect and music ambiance

--- public/js/game.js
+++ public/js/game.js
@@ -6,13 +6,15 @@ var padding = .12;
 
 canvas.width = canvas.clientWidth
 canvas.height = canvas.clientHeight
-ctx.width = canvas.clientWidth
+ctx.width = canvas.clientWidth 
 ctx.height = canvas.clientHeight
 
 var cellSize = Math.floor(canvas.clientWidth/cote);
 
 var flag = false;
 var dict = {};
+
+var recursive = 0
 
 function generate(){
     for (let i = 0; i <= cote+1; i++) {
@@ -109,6 +111,26 @@ function calculateGenerationStep(){
         for (let j = 0; j <= cote+1; j++) {
             nxtDict[cote+1].push(undefined)
         }
+
+
+
+    var diff = false;
+    recursive++
+    for(let i = 1; i <=cote; i++){
+        for(let j = 1; j <=cote; j++){
+            if(dict[i][j]!==nxtDict[i][j]){
+                recursive = 0
+                diff = true
+                break
+            }
+        }
+    }
+
+
+    var audio;
+    diff? audio = new Audio('/audio/remove.mp3') : audio = new Audio('/audio/norun.mp3')  
+    audio.volume = 0.2
+    audio.play();
     
     dict = nxtDict
     return dict
@@ -117,7 +139,16 @@ function calculateGenerationStep(){
 
 function runGame(){
 
+    if(recursive>20){
+        flag=false
+        recursive = 0
+        var audio = new Audio('/audio/start.mp3');
+        audio.play();
+    }
+
     if(flag){setTimeout(()=>{runGame();}, 50)}
+
+    
     
     calculateGenerationStep()
     renderFrame()
@@ -162,8 +193,17 @@ canvas.addEventListener("mousedown", function(e)
 {
     getMousePosition(canvas, e)
 
-    dict[mouseX][mouseY]==undefined? dict[mouseX][mouseY] = true : dict[mouseX][mouseY] = undefined
+    if(dict[mouseX][mouseY]==undefined){
+        dict[mouseX][mouseY] = true;
+        var audio = new Audio('/audio/add.mp3');
+        audio.volume = 0.2
+    } else {
+        dict[mouseX][mouseY] = undefined
+        var audio = new Audio('/audio/remove.mp3');
+    } 
+    audio.play(); 
     renderFrame()
+    
 });
 
 window.addEventListener('keydown', function(e) {
@@ -176,15 +216,28 @@ document.addEventListener('keydown', function(event) {
     if(event.key == ' ') {
         flag = !flag
         flag? runGame() : undefined
+        var audio = new Audio('/audio/start.mp3');
+        audio.play();
     }
     if(event.key == 'r') {
         randomize()
         renderFrame()
+
+        for (let s = 0; s < 4; s++) {
+            setTimeout(()=>{
+                var audio = new Audio('/audio/add.mp3');
+                audio.volume = 0.4
+                audio.play()}, s*33)
+        }
     }
     if(event.key == 'Delete' || event.key == 'c') {
         generate()
         renderFrame()
         flag = false
+
+        var audio = new Audio('/audio/erase.mp3');
+        audio.volume = 0.4
+        audio.play();
     }
     if(event.key == '+') {
         padding = Math.min(0.24, padding+0.02)
