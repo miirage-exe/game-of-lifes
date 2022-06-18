34812055ce1eb75e99a04cc6ac59bea60f6480eb
miirage <mika.baeza@laposte.net>
2022-06-18 16:02:48 +0200

    better interface for canvas

--- public/js/game.js
+++ public/js/game.js
@@ -1,33 +1,60 @@
 var canvas = document.getElementById('game-canvas');
 var ctx = canvas.getContext('2d');
 
-
 const cote = 64
-const cellSize = 2
+const cellSize = 8
 
-function showdict(){
-    console.clear()
-    for (let i = 0; i <= cote+1; i++) {
-        console.log(dict[i])
-    }
-}
+ctx.width = cote*cellSize
+ctx.height = cote*cellSize
 
-// Genère le dict
-var dict = {}
-for (let i = 0; i < cote+2; i++) {
-    dict[i] = []
-        for (let j = 0; j < cote+2; j++) {
-            if(j%2){
-                dict[i].push(false)
-            } else {
-                dict[i].push(true)
-            }
+var flag = false
+var dict = {};
+
+
+
+
+function randomize(){
+
+    //first colonne 
+        // i = 0
+        dict[0] = []
+        for (let j = 0; j <= cote+1; j++) {
+            dict[0].push(undefined)
+        }
+
+
+        for (let i = 1; i <= cote; i++) {
+
+            dict[i] = []
+            dict[i].push(undefined)
+
+                for (let j = 1; j <= cote; j++) {
+
+                    switch(Math.floor(Math.random()*5)){
+                        case 2:
+                            dict[i].push(true)
+                        // case 4:
+                        //     dict[i].push(false)
+                        default:
+                            dict[i].push(undefined)
+                    }
+                }
+
+            dict[i].push(undefined)
+        }
+
+        //last colonne
+        // i = cote+1
+        dict[cote+1] = []
+        for (let j = 0; j <= cote+1; j++) {
+            dict[cote+1].push(undefined)
         }
 }
 
-showdict()
 
-function runStep(){
+
+
+function calculateGenerationStep(){
     var nxtDict = {}
     var p =0
 
@@ -35,46 +62,42 @@ function runStep(){
     // i = 0
     nxtDict[0] = []
         for (let j = 0; j <= cote+1; j++) {
-            nxtDict[0].push(false)
+            nxtDict[0].push(undefined)
         }
 
 
     for (let i = 1; i <= cote; i++) {
 
         nxtDict[i] = []
-        nxtDict[i].push(false)
+        nxtDict[i].push(undefined)
 
             for (let j = 1; j <= cote; j++) {
                 //calcul population
                 p = 0
                 for (let ik = i-1; ik <= i+1; ik++) {
                     for (let jk = j-1; jk <= j+1; jk++) {                        
-                        if(dict[ik][jk]){
+                        if(dict[ik][jk] !== undefined){
                             p++
                         }
                     }
                 }
-                if(dict[i][j]){
-                    if(p==3 || p==4){
-                        nxtDict[i].push(true)
-                    } else {
-                        nxtDict[i].push(false)
-                    }
-                } else if(p==3){
+                if(p===3){
+                    nxtDict[i].push(true)
+                } else if (dict[i][j] !== undefined && p === 4){
                     nxtDict[i].push(true)
                 } else {
-                    nxtDict[i].push(false)
+                    nxtDict[i].push(undefined)
                 }
             }
 
-        nxtDict[i].push(false)
+        nxtDict[i].push(undefined)
     }
 
     //last colonne
     // i = cote+1
     nxtDict[cote+1] = []
         for (let j = 0; j <= cote+1; j++) {
-            nxtDict[cote+1].push(false)
+            nxtDict[cote+1].push(undefined)
         }
     
     dict = nxtDict
@@ -82,30 +105,32 @@ function runStep(){
 }
 
 
-var frame = 0
 function runGame(){
-    if(frame<100){
-        frame++
-        setTimeout(()=>{runGame()}, 10)
-        console.log('salut mon pote')
-    }
-    runStep()
-    drawGrid()
+
+    if(flag){setTimeout(()=>{runGame();}, 20)}
+    
+    calculateGenerationStep()
+    renderFrame()
 }
 
-function drawGrid(){
+function renderFrame(){
     for (let i = 1; i <= cote; i++) {
         for (let j = 1; j <= cote; j++) {
-            if(dict[i][j]){
+            if(dict[i][j] === undefined){
+                if((i+j)%2){
+                    ctx.fillStyle = '#EDEDED';
+                } else {
+                    ctx.fillStyle = 'white';
+                }
+            } else if(dict[i][j] === false){
+                ctx.fillStyle = 'red';
+            }else {
                 ctx.fillStyle = 'black';
-            } else {
-                ctx.fillStyle = 'white';
             }
             ctx.fillRect((i-1)*cellSize, (j-1)*cellSize, cellSize, cellSize);
         }
     }
 }
 
-
-ctx.width = 800
-ctx.height = 800
+clear()
+renderFrame()
