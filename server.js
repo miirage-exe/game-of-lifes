be6c30e81a952bad510b8dcbb9dacdc1a473ec39
miirage <mika.baeza@laposte.net>
2022-06-26 12:51:00 +0200

    implemented sessions and lot of stuff

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const express = require('express')
 const http = require('http')
 const port = process.env.PORT
+const session = require('express-session')
+const webSocket = require('./webSocket/index.js')
 const { randomUUID } = require('crypto');
+const userSession = require('./presets/userSession.js');
+const { equal } = require('assert');
 
 const app = express()
 const server = http.createServer(app)
@@ -16,51 +20,116 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// Sessions ===================
+
+const sessionMiddleware = session({
+    secret: randomUUID(),  
+    saveUninitialized: true,
+    resave: true,
+    extended: true,
+    cookie: { secure: false }
+  })
+
+// Session
+app.use(sessionMiddleware)
+
+// connect middleware to a Socket.IO middleware
+webSocket(server, sessionMiddleware)
 
 
+
+// Routes ====================================================================
 
 
 
 // Basics =====================
-
+// GET /
 app.get('/', (req, res) => {
     res.render('home')
 })
+
+// GET /portal
+app.get('/portal', (req, res) => {
+    res.render('portal')
+})
+
+// POST /portal
+app.post('/portal', (req, res) => {
+
+    req.session._user? undefined : req.session._user = new userSession()
+
+    res.redirect('/lobby')
+})
+
+// GET /home
 app.get('/home', (req, res) => {
     res.redirect('/')
 })
 
+// GET /workbench
 app.get('/workbench', (req, res) => {
     res.render('workbench')
 })
 
+// GET /rules
 app.get('/rules', (req, res) => {
     res.render('rules')
 })
 
+// GET /settings
 app.get('/settings', (req, res) => {
     res.render('settings')
 })
 
-// Lobby =====================
 
+
+
+
+// Lobby =====================
+// GET /lobby
 app.get('/lobby', (req, res) => {
     res.render('lobby')
 })
 
-// Rooms =====================
 
+
+
+
+// Rooms =====================
+// GET /room/join
 app.get('/room/join', (req, res) => {
     res.render('room/join')
 })
 
+// POST /room/join
+app.post('/room/join', (req, res) => {
+    if(!req.session._user){
+        res.redirect('/portal')
+    } else if(!req.body.token){
+        res.render('room/join')
+    } else {
+        res.redirect(`/room/${req.body.token}`)
+    }
+})
+
+// GET /room/create
 app.get('/room/create', (req, res) => {
     res.render('room/create')
 })
 
+// GET /room/:token
 app.get('/room/:roomToken', (req, res) => {
-    res.render('room/index', {id: req.params.roomToken})
+    if (!req.session._user){
+        res.redirect('/portal')
+    } else {
+        res.render('room/index', {id: req.params.roomToken})
+    }
+    
 })
+
+
+
+
 
 // Game =====================
 
